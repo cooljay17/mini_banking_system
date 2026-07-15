18abd9993dea91e2f138818faa16c91ad5a3c8f9
Jayanthi Manickam Sethu <coolj1784@gmail.com>
2026-07-16 00:44:38 +0530

    Delete scripts/03_pkg_banking_system_declaration.sql

--- scripts/03_pkg_banking_system_declaration.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE PACKAGE pkg_banking_system AS    
-PROCEDURE open_account(p_customer_id NUMBER,
-p_branch_code VARCHAR2,
-p_account_type VARCHAR2, 
-p_opening_balance OUT NUMBER, 
-p_account_id OUT NUMBER);
-
-END pkg_banking_system; 
-/
